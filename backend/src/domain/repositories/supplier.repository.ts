d386d35d9f6dfc17637b479fed4947006b2dadc3
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2026-01-07 19:44:18 -0300

    CORREÇÃO SUPPPLIER E ENTITY

--- backend/src/domain/repositories/supplier.repository.ts
+++ backend/src/domain/repositories/supplier.repository.ts
@@ -1,8 +1,8 @@
-import { SupplierEntity } from "../../infra/db/typeorm/entities/supplier.entity";
+import { Supplier } from "../entities/supplier";
 
 export interface ISupplierRepository {
-  create(data: Partial<SupplierEntity>): Promise<SupplierEntity>;
-  findAll(): Promise<SupplierEntity[]>;
-  findById(id: string): Promise<SupplierEntity | null>;
-  update(id: string, data: Partial<SupplierEntity>): Promise<SupplierEntity>;
+  create(data: Partial<Supplier>): Promise<Supplier>;
+  findAll(): Promise<Supplier[]>;
+  findById(id: string): Promise<Supplier | null>;
+  update(id: string, data: Partial<Supplier>): Promise<Supplier>;
 }
--- backend/src/infra/db/typeorm/repositories/supplier.repository.ts
+++ backend/src/infra/db/typeorm/repositories/supplier.repository.ts
@@ -2,15 +2,16 @@ import { Repository } from "typeorm";
 import { SupplierEntity } from "../entities/supplier.entity";
 import { AppDataSource } from "../database";
 import { ISupplierRepository } from "../../../../domain/repositories/supplier.repository";
+import { Supplier } from "../../../../domain/entities/supplier";
 
 export class SupplierRepository implements ISupplierRepository {
-  private ormRepo: Repository<SupplierEntity>;
+  private ormRepo: Repository<Supplier>;
 
   constructor() {
     this.ormRepo = AppDataSource.getRepository(SupplierEntity);
   }
 
-  async create(data: Partial<SupplierEntity>) {
+  async create(data: Partial<Supplier>) {
     const supplier = this.ormRepo.create(data);
     return this.ormRepo.save(supplier);
   }
@@ -23,8 +24,8 @@ export class SupplierRepository implements ISupplierRepository {
     return this.ormRepo.findOne({ where: { id } });
   }
 
-  async update(id: string, data: Partial<SupplierEntity>) {
+  async update(id: string, data: Partial<Supplier>) {
     await this.ormRepo.update(id, data);
-    return this.findById(id) as Promise<SupplierEntity>;
+    return this.findById(id) as Promise<Supplier>;
   }
 }
